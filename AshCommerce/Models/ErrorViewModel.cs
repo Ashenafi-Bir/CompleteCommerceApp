febed8590fdc53e0ac9b14b5226596337a654f95
Ashenafi-Bir <Ashenafibirhanu33@gmail.com>
2024-06-06 09:33:54 +0300

    Add project files.

---
+++ AshCommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AshCommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
